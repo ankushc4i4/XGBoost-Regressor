d1eb1be7afce6f6f9f13768271504724d05c056b
ankushc4i4 <107626656+ankushc4i4@users.noreply.github.com>
2022-06-16 18:21:34 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# MLflow XGBoost Regressor
+
+This sample project trains a regression tree using [XGBoost](https://github.com/dmlc/xgboost) and makes it available as an [MLflow](http://mlflow.org) project.
---
+++ main.py
@@ -0,0 +1,67 @@
+import click
+import pandas as pd
+import numpy as np
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import *
+import xgboost as xgb
+import mlflow
+import mlflow.sklearn
+
+
+def eval_metrics(actual, pred):
+    rmse = np.sqrt(mean_squared_error(actual, pred))
+    mae = mean_absolute_error(actual, pred)
+    r2 = r2_score(actual, pred)
+    return (rmse, mae, r2)
+
+
+@click.command()
+@click.option("--training_data")
+@click.option("--test_data")
+@click.option("--label_col")
+@click.option("--max_depth", default=7)
+@click.option("--ntrees", default=200)
+@click.option("--lr", default=0.005)
+def main(training_data, test_data, label_col, max_depth, ntrees, lr):
+    trainDF = pd.read_parquet(training_data)
+    testDF = pd.read_parquet(test_data)
+    yTrain = trainDF[[label_col]]
+    XTrain = trainDF.drop([label_col], axis=1)
+    yTest = testDF[[label_col]]
+    XTest = testDF.drop([label_col], axis=1)
+
+    xgbRegressor = xgb.XGBRegressor(
+        max_depth=max_depth,
+        n_estimators=ntrees,
+        learning_rate=lr,
+        random_state=42,
+        seed=42,
+        subsample=0.75,
+        colsample_bytree=0.75,
+        reg_lambda=1,
+        gamma=1)
+    pipeline = Pipeline(steps=[("regressor", xgbRegressor)])
+
+    pipeline.fit(XTrain, yTrain)
+    yPred = pipeline.predict(XTest)
+    
+    (rmse, mae, r2) = eval_metrics(yTest, yPred)
+    
+    print("XGBoost tree model (max_depth=%f, trees=%f, lr=%f):" % (max_depth, ntrees, lr))
+    print("  RMSE: %s" % rmse)
+    print("  MAE: %s" % mae)
+    print("  R2: %s" % r2)
+    
+    mlflow.log_param("max_depth", max_depth)
+    mlflow.log_param("ntrees", ntrees)
+    mlflow.log_param("lr", lr)
+    mlflow.log_metric("RMSE", rmse)
+    mlflow.log_metric("R2", r2)
+    mlflow.log_metric("MAE", mae)
+    
+    mlflow.sklearn.log_model(pipeline, "model")
+    #print("Model saved in run %s" % mlflow.active_run_id())
+
+
+if __name__ == "__main__":
+    main()
